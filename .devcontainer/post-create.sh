39225f246f843b887898aeeda8218ee6cc151f41
Ttrần Pphú <38020975+TtranPphu@users.noreply.github.com>
2025-03-14 04:10:53 +0000

    Update devcontainer setup: streamline zsh configuration and enhance Powerlevel10k theme integration

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,9 +1,3 @@
 . $HOME/.local/bin/env && uv sync
-
-echo >> ~/.zshrc
-echo "# devcontainer configuration" >> ~/.zshrc 
-echo source $HOME/.local/bin/env >> ~/.zshrc
-echo source /workspaces/diu-thi-hieu/.venv/bin/activate >> ~/.zshrc
-echo export HISTFILE="/workspaces/diu-thi-hieu/.zsh_history" >> ~/.zshrc
-echo export HISTSIZE=1000 >> ~/.zshrc
-echo export SAVEHIST=10000 >> ~/.zshrc
+cp .devcontainer/.zshrc ~/.zshrc
+cp .devcontainer/.p10k.zsh ~/.p10k.zsh
